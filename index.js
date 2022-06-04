dc5148f0b7a01f03cd917c0343d2db4ed2f48ed5
elzarsadybekov <elzar.sadybekov@gmail.com>
2022-06-04 16:22:15 +0600

    initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+// let a = 10
+// let b = 3
+// console.log(a%b);
+
+// const a = "1";
+// const b = "2";
+// const c = "белых медведей";
+// const sum = a + b + c;
+// console.log(sum);
+
+// let a = 5;
+// let b = 10;
+// let c = ++a + --b;
+// console.log(c)
+
+// const x = 15 * ((4 + 25) -55);
+// console.log('Expected value: -390', x);
+
+// const height = 23;
+// const weight = 10;
+// const s = height * weight;
+// console.log(s);
+
+const heightC = 10;
+const dC = 4;
+const v = heightC * 3.14 * dC ** 2 / 4;
+console.log(v);
